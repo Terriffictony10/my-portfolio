0772578266312a23dd8946bd75011bf4cc81307b
Tony Yonan <superzman15@gmail.com>
2025-02-04 00:31:35 -0700

    asklfjasdlkj

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -188,7 +188,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4bafcfd1fca1ac1c"; }
+/******/ 		__webpack_require__.h = function() { return "a6ecd68e60cbc3a4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import { cookieStorage, createStorage } from '@wagmi/core';
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
 import { mainnet, arbitrum } from '@reown/appkit/networks';
 
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+export const projectId = '6df9df8b72567f05d2f0d1503b13538f';
 
 if (!projectId) {
   throw new Error('Project ID is not defined');
