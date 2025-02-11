d866747956ce857a2c44f51e2d2ddfbbeabd48ed
Tony Yonan <superzman15@gmail.com>
2025-02-11 16:23:47 -0700

    asdasflkl

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <Head>
+        <meta name="viewport" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
