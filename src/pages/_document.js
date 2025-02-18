ae97fbdb3b15d36070cfb3628350e9ed47d58b0a
Tony Yonan <superzman15@gmail.com>
2025-02-17 17:27:17 -0700

    aslasldfkf

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="viewport" />
+        {/* This sets the viewport width to 400px and the initial scale to 1 */}
+        <meta name="viewport" content="width=400, initial-scale=1" />
       </Head>
       <body className="antialiased">
         <Main />
